2bd19b00b73c76d032902db96f470e8e9233acf4
suman <sumanbogati@gmail.com>
2016-11-20 00:40:49 +0530

    make indenting correct

--- worker/example/light/main.js
+++ worker/example/light/main.js
@@ -1,27 +1,27 @@
 if(window.Worker){
 	
-// the data should be in object format
-var car = {
-	model : 'GLA-200',
-	color : 'White',
-	price : null
-}
+	// the data should be in object format
+	var car = {
+		model : 'GLA-200',
+		color : 'White',
+		price : null
+	}
 
-// the data would pass to the worker
-anyWorker.postMessage(car); 
-	
-// received data with onmessage function
-// Whatever object is passed by worker can be found
-// at event.data
+	// the data would pass to the worker
+	anyWorker.postMessage(car); 
+		
+	// received data with onmessage function
+	// Whatever object is passed by worker can be found
+	// at event.data
 
-anyWorker.onmessage = function (e){
-	// get modified object from worker
-	var mcar = e.data; 
-	console.log(
-		'Car model is ' + mcar.model + '\n' + 
-		'Car color is ' + mcar.color + '\n' + 
-		'Car price is ' + mcar.price + '\n' + 
-		'Car company is ' + mcar.company + '\n' 
-	);
-}
+	anyWorker.onmessage = function (e){
+		// get modified object from worker
+		var mcar = e.data; 
+		console.log(
+			'Car model is ' + mcar.model + '\n' + 
+			'Car color is ' + mcar.color + '\n' + 
+			'Car price is ' + mcar.price + '\n' + 
+			'Car company is ' + mcar.company + '\n' 
+		);
+	}
 }
